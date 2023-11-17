8c577b3b3396b0cbe1b7cee1bb0bbb672e58c5a6
rezatb <103198863+rezatb@users.noreply.github.com>
2023-11-17 05:19:05 +0330

    test_case_1 passed

--- main.py
+++ main.py
@@ -3,69 +3,68 @@ from collections import deque
 class Node:
     # initial function
     def __init__(self, pos, value, parent=None):
-        self.pos = pos
-        self.value = value
-        self.parent = parent
+        self.pos = pos # the position of node
+        self.value = value # the value of node
+        self.parent = parent # parent of each node, root parent is None
 
 
-# read the whole Maze
+# read the file
 def read_maze():
-    file = open("Maze_env.txt")
-    lines = file.readlines()
-    # get the grid size
-    m, n = lines[0].split(',')
-    # read the map
-    maze_map = []
-    maze_map.extend([[char for char in line.strip()] for line in lines[1:]])
-    return (m, n), maze_map
+    file = open("Test Case/test_case_2.txt") # read file
+    lines = file.readlines() # exract lines
+    m, n = lines[0].split(',') # get the grid size
+    map = []
+    map.extend([[char for char in line.strip()] for line in lines[1:]]) # store map
+    return (m, n), map
 
 
-# find the start and the goal
+# find start and goal node
 def find_S_G(grid_size, maze_map):
     m, n = int(grid_size[0]), int(grid_size[1])
     print(maze_map)
-    g_pos = [(i, j) for i in range(m) for j in range(n) if
+    goal_pos = [(i, j) for i in range(m) for j in range(n) if
              maze_map[i][j] == 'G'][0]
-    s_pos = [(i, j) for i in range(m) for j in range(n) if
+    start_pos = [(i, j) for i in range(m) for j in range(n) if
              maze_map[i][j] == 'S'][0]
-    return s_pos, g_pos
+    return start_pos, goal_pos
 
 
-def bfs(s_pos, g_pos, maze_map, grid_size):
-    grid_x, grid_y = int(grid_size[0]), int(grid_size[1])
+# bfs algorithm
+def bfs(start_pos, goal_pos, maze_map, grid_size):
+    grid_x, grid_y = int(grid_size[0]), int(grid_size[1]) # extract size of the grid
     moves = {
-        'R': (1, 0),
-        'L': (-1, 0),
-        'U': (0, -1),
-        'D': (0, 1),
+        'R': (0, 1),
+        'L': (0, -1),
+        'U': (-1, 0),
+        'D': (1, 0),
     }
-    node = Node(s_pos, "S")
+    node = Node(start_pos, "S")
     queue = deque()
-    moves_list = []
+    stored_moves = []
     queue.append(node)
     for item in queue:
         print(item.value)
     visited_blocks = [[False for i in range(grid_x)]
                       for j in range(grid_y)]
-    visited_blocks[int(s_pos[0])][int(s_pos[1])] = True
+    visited_blocks[int(start_pos[0])][int(start_pos[1])] = True
 
     while queue:
         current_node = queue.popleft()  # Dequeue the front node
-        print(current_node.pos, g_pos)
-        if current_node.pos == g_pos:  # check if we reached the goal
+        print(current_node.pos, goal_pos)
+        if current_node.pos == goal_pos:  # check if we reached the goal
             while current_node.parent is not None:
                 # print(current_node.parent.pos)
-                dy, dx = current_node.pos[0] - current_node.parent.pos[0], current_node.pos[1] - current_node.parent.pos[1]
+                dx, dy = current_node.pos[0] - current_node.parent.pos[0], current_node.pos[1] - current_node.parent.pos[1]
                 if (dx, dy) == moves['R']:
-                    moves_list.append('R')
+                    stored_moves.append('R')
                 elif (dx, dy) == moves['L']:
-                    moves_list.append('L')
+                    stored_moves.append('L')
                 elif (dx, dy) == moves['U']:
-                    moves_list.append('U')
+                    stored_moves.append('U')
                 elif (dx, dy) == moves['D']:
-                    moves_list.append('D')
+                    stored_moves.append('D')
                 current_node = current_node.parent
-            return moves_list[::-1]
+            return stored_moves[::-1]
 
         # x, y = int(current_node.pos[0]), int(current_node.pos[1])
         # if not visited_blocks[x][y]:
@@ -77,10 +76,11 @@ def bfs(s_pos, g_pos, maze_map, grid_size):
             new_y_pos = y_pos + move[1]
             # print(new_x_pos, new_y_pos)
             if grid_x > new_x_pos >= 0 and grid_y > new_y_pos >= 0:
+                print(new_x_pos, new_y_pos, len(visited_blocks), len(visited_blocks[0]))
                 if maze_map[new_x_pos][new_y_pos] == "-" or maze_map[new_x_pos][new_y_pos] == "G":
                     if not visited_blocks[new_x_pos][new_y_pos]:
                         node = Node((new_x_pos, new_y_pos),
-                                        maze_map[new_x_pos][new_y_pos], node)
+                                    maze_map[new_x_pos][new_y_pos], node)
                         visited_blocks[new_x_pos][new_y_pos] = True
                         queue.append(node)
                         print(node.value)
