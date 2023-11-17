7cd23e2aa76105106e3ae101ecb0714e44c6b562
rezatb <103198863+rezatb@users.noreply.github.com>
2023-11-17 04:37:45 +0330

    third commit

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from collections import deque
 
 class Node:
     # initial function
-    def __init__(self, pos, value):
+    def __init__(self, pos, value, parent=None):
         self.pos = pos
         self.value = value
+        self.parent = parent
 
 
 # read the whole Maze
@@ -24,9 +25,9 @@ def find_S_G(grid_size, maze_map):
     m, n = int(grid_size[0]), int(grid_size[1])
     print(maze_map)
     g_pos = [(i, j) for i in range(m) for j in range(n) if
-             maze_map[i][j] == 'G']
+             maze_map[i][j] == 'G'][0]
     s_pos = [(i, j) for i in range(m) for j in range(n) if
-             maze_map[i][j] == 'S']
+             maze_map[i][j] == 'S'][0]
     return s_pos, g_pos
 
 
@@ -35,44 +36,57 @@ def bfs(s_pos, g_pos, maze_map, grid_size):
     moves = {
         'R': (1, 0),
         'L': (-1, 0),
-        'U': (0, 1),
-        'D': (0, -1),
+        'U': (0, -1),
+        'D': (0, 1),
     }
-    starting_node = Node(s_pos, "S")
+    node = Node(s_pos, "S")
     queue = deque()
     moves_list = []
-    queue.append(starting_node)
+    queue.append(node)
+    for item in queue:
+        print(item.value)
+    visited_blocks = [[False for i in range(grid_x)]
+                      for j in range(grid_y)]
+    visited_blocks[int(s_pos[0])][int(s_pos[1])] = True
 
     while queue:
         current_node = queue.popleft()  # Dequeue the front node
+        print(current_node.pos, g_pos)
         if current_node.pos == g_pos:  # check if we reached the goal
-            print(str(moves_list))
-            return None
+            while current_node.parent is not None:
+                # print(current_node.parent.pos)
+                dy, dx = current_node.pos[0] - current_node.parent.pos[0], current_node.pos[1] - current_node.parent.pos[1]
+                if (dx, dy) == moves['R']:
+                    moves_list.append('R')
+                elif (dx, dy) == moves['L']:
+                    moves_list.append('L')
+                elif (dx, dy) == moves['U']:
+                    moves_list.append('U')
+                elif (dx, dy) == moves['D']:
+                    moves_list.append('D')
+                current_node = current_node.parent
+            return moves_list[::-1]
 
-            # for i in range(4):
-        visited_blocks = [[False for i in range(grid_x)]
-                          for j in range(grid_y)]
-        visited_blocks[int(s_pos[0])][int(s_pos[1])] = True
-        x, y = int(current_node.pos[0]), int(current_node.pos[1])
-        if not visited_blocks[x][y]:
-            visited_blocks[x][y] = True
+        # x, y = int(current_node.pos[0]), int(current_node.pos[1])
+        # if not visited_blocks[x][y]:
+        #     visited_blocks[x][y] = True
         x_pos = current_node.pos[0]
         y_pos = current_node.pos[1]
-        top_edge_flag = False
-        bottom_edge_flag = False
-
         for move in moves.values():
             new_x_pos = x_pos + move[0]
             new_y_pos = y_pos + move[1]
-            if new_x_pos < grid_x and new_y_pos < grid_y and new_x_pos >= 0 and new_y_pos >= 0:
-                if maze_map[new_x_pos][new_y_pos] == 1:
+            # print(new_x_pos, new_y_pos)
+            if grid_x > new_x_pos >= 0 and grid_y > new_y_pos >= 0:
+                if maze_map[new_x_pos][new_y_pos] == "-" or maze_map[new_x_pos][new_y_pos] == "G":
                     if not visited_blocks[new_x_pos][new_y_pos]:
-                        new_node = Node((new_y_pos, new_y_pos),
-                                        maze_map[new_x_pos][new_y_pos])
+                        node = Node((new_x_pos, new_y_pos),
+                                        maze_map[new_x_pos][new_y_pos], node)
                         visited_blocks[new_x_pos][new_y_pos] = True
-                        queue.append(new_node)
-
+                        queue.append(node)
+                        print(node.value)
 
 if __name__ == '__main__':
     grid_size, maze_map = read_maze()
     s_pos, g_pos = find_S_G(grid_size, maze_map)
+    moves_list = bfs(s_pos, g_pos, maze_map, grid_size)
+    print(''.join(moves_list))
