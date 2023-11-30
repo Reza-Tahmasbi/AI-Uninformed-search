5074ee566c0381a5ea021597645afaae7a6cf12f
Reza <rezatb2002@gmail.com>
2023-11-30 21:39:10 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,92 +1,42 @@
-from collections import deque
-
-class Node:
-    # initial function
-    def __init__(self, pos, value, parent=None):
-        self.pos = pos # the position of node
-        self.value = value # the value of node
-        self.parent = parent # parent of each node, root parent is None
+from draw_maze import draw_maze
+from mazegraph import MazeGraph
 
 
-# read the file
-def read_maze():
-    file = open("Test Case/test_case_2.txt") # read file
-    lines = file.readlines() # exract lines
-    m, n = lines[0].split(',') # get the grid size
-    map = []
-    map.extend([[char for char in line.strip()] for line in lines[1:]]) # store map
-    return (m, n), map
+def read_maze(file_name):
+    file = open(file_name)  # read file
+    lines = file.readlines()  # extract lines
+    m, n = lines[0].split(',')  # get the grid size
+    maze_map = []
+    maze_map.extend([line.replace("\n","") for line in lines[1:]])  # store map
+    n = str(n.replace("\n", ""))
+    return int(m), int(n), maze_map
 
 
-# find start and goal node
-def find_S_G(grid_size, maze_map):
-    m, n = int(grid_size[0]), int(grid_size[1])
-    print(maze_map)
-    goal_pos = [(i, j) for i in range(m) for j in range(n) if
-             maze_map[i][j] == 'G'][0]
-    start_pos = [(i, j) for i in range(m) for j in range(n) if
-             maze_map[i][j] == 'S'][0]
-    return start_pos, goal_pos
+def find_start_goal(rows, cols):
+    start_node = None
+    goal_node = None
+    # Find the starting and goal positions
+    for i in range(rows):
+        for j in range(cols):
+            if maze_graph.maze[i][j] == "S":
+                start_node = i * cols + j
+            elif maze_graph.maze[i][j] == "G":
+                goal_node = i * cols + j
+    return start_node, goal_node
 
-
-# bfs algorithm
-def bfs(start_pos, goal_pos, maze_map, grid_size):
-    grid_x, grid_y = int(grid_size[0]), int(grid_size[1]) # extract size of the grid
-    moves = {
-        'R': (0, 1),
-        'L': (0, -1),
-        'U': (-1, 0),
-        'D': (1, 0),
-    }
-    node = Node(start_pos, "S")
-    queue = deque()
-    stored_moves = []
-    queue.append(node)
-    for item in queue:
-        print(item.value)
-    visited_blocks = [[False for i in range(grid_x)]
-                      for j in range(grid_y)]
-    visited_blocks[int(start_pos[0])][int(start_pos[1])] = True
-
-    while queue:
-        current_node = queue.popleft()  # Dequeue the front node
-        print(current_node.pos, goal_pos)
-        if current_node.pos == goal_pos:  # check if we reached the goal
-            while current_node.parent is not None:
-                # print(current_node.parent.pos)
-                dx, dy = current_node.pos[0] - current_node.parent.pos[0], current_node.pos[1] - current_node.parent.pos[1]
-                if (dx, dy) == moves['R']:
-                    stored_moves.append('R')
-                elif (dx, dy) == moves['L']:
-                    stored_moves.append('L')
-                elif (dx, dy) == moves['U']:
-                    stored_moves.append('U')
-                elif (dx, dy) == moves['D']:
-                    stored_moves.append('D')
-                current_node = current_node.parent
-            return stored_moves[::-1]
-
-        # x, y = int(current_node.pos[0]), int(current_node.pos[1])
-        # if not visited_blocks[x][y]:
-        #     visited_blocks[x][y] = True
-        x_pos = current_node.pos[0]
-        y_pos = current_node.pos[1]
-        for move in moves.values():
-            new_x_pos = x_pos + move[0]
-            new_y_pos = y_pos + move[1]
-            # print(new_x_pos, new_y_pos)
-            if grid_x > new_x_pos >= 0 and grid_y > new_y_pos >= 0:
-                print(new_x_pos, new_y_pos, len(visited_blocks), len(visited_blocks[0]))
-                if maze_map[new_x_pos][new_y_pos] == "-" or maze_map[new_x_pos][new_y_pos] == "G":
-                    if not visited_blocks[new_x_pos][new_y_pos]:
-                        node = Node((new_x_pos, new_y_pos),
-                                    maze_map[new_x_pos][new_y_pos], node)
-                        visited_blocks[new_x_pos][new_y_pos] = True
-                        queue.append(node)
-                        print(node.value)
 
 if __name__ == '__main__':
-    grid_size, maze_map = read_maze()
-    s_pos, g_pos = find_S_G(grid_size, maze_map)
-    moves_list = bfs(s_pos, g_pos, maze_map, grid_size)
-    print(''.join(moves_list))
+    file_name = "Test Case/test_case_1.txt"
+    rows, cols, maze = read_maze(file_name)
+    maze_graph = MazeGraph(rows, cols, maze)
+    start_node, goal_node = find_start_goal(rows, cols)
+    # Find the path from 'S' to 'G'
+    path = maze_graph.bfs(start_node, goal_node)
+
+    if path:
+        print("Path from 'S' to 'G':", path)
+        directions = maze_graph.get_directions(path)
+        print("Directions:", directions)
+        draw_maze(maze_graph.maze, path)
+    else:
+        print("No path found from 'S' to 'G'")
